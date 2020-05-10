7c67dbd1526b66e9c824fd138b7af5fd584514a0
mnwokedi <60416811+mnwokedi@users.noreply.github.com>
2020-05-10 16:38:49 -0700

    4

--- CPjs.js
+++ CPjs.js
@@ -1,25 +1,57 @@
-var fields = {};
-document.getElementById("form").submit();
-document.addEventListener("DOMContentLoaded", function () {
-  fields.Name = document.getElementById("Name");
+const tableKey="cms-table";
+let clearBtn = document.getElementById("clearBtn");
+clearBtn.addEventListener;
 
-  fields.email = document.getElementById("email");
-
-  fields.Number = document.getElementById("Number");
+("click",(); =>{
+  localStorage.removeItem(tableKey);
 });
-function isNotEmpty(value) {
-  if (value == null || typeof value == "undefined") return false;
-  return value.length > 0;
-}
-function fieldValidation(field, validationFunction) {
-  if (field == null) return false;
 
-  let isFieldValid = validationFunction(field.value);
-  if (!isFieldValid) {
-    field.className = "placeholderRed";
-  } else {
-    field.className = "";
+let cmsTable;
+let cmsTableDemo = {
+  'mis': {
+    'email':'m@matchMedia.com',
+    'phone': '552-858-8955',
   }
+};
 
-  return isFieldValid;
+let refreshDOMTable = () => {
+  let cmsTableKeys = Object.keys(cmsTable);
+  let tableContainer = document.getElementById('cmsTableContainer')
+  let oldTablebody = document.getElementById('tablebody');
+  tableContainer.removeChild(oldTablebody);
+  let newTableBody = document.createElement('span');
+newTableBody.id = 'tablebody';
 }
+
+
+// var fields = {};
+// document.getElementById("form").submit();
+// document.addEventListener("DOMContentLoaded", function () {
+//   fields.Name = document.getElementById("name");
+
+//   fields.email = document.getElementById("email");
+
+//   fields.Number = document.getElementById("number");
+// });
+// function isNotEmpty(value) {
+//   if (value == null || typeof value == "undefined") return false;
+//   return value.length > 0;
+// }
+// function fieldValidation(field, validationFunction) {
+//   if (field == null) return false;
+
+//   let isFieldValid = validationFunction(field.value);
+//   if (!isFieldValid) {
+//     field.className = "placeholderRed";
+//   } else {
+//     field.className = "";
+//   }
+
+//   return isFieldValid;
+// }
+// const person = {
+//   name: "name",
+//   location: "name",
+// };
+
+// window.localStorage.setItem("user", JSON.stringify(person));
