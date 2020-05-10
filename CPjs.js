fdd8ac2c56f3a09b5d97f32b74fc3fa50092ef60
mnwokedi <60416811+mnwokedi@users.noreply.github.com>
2020-05-09 21:56:00 -0700

    3

--- CPjs.js
+++ CPjs.js
@@ -1,1 +1,25 @@
+var fields = {};
 document.getElementById("form").submit();
+document.addEventListener("DOMContentLoaded", function () {
+  fields.Name = document.getElementById("Name");
+
+  fields.email = document.getElementById("email");
+
+  fields.Number = document.getElementById("Number");
+});
+function isNotEmpty(value) {
+  if (value == null || typeof value == "undefined") return false;
+  return value.length > 0;
+}
+function fieldValidation(field, validationFunction) {
+  if (field == null) return false;
+
+  let isFieldValid = validationFunction(field.value);
+  if (!isFieldValid) {
+    field.className = "placeholderRed";
+  } else {
+    field.className = "";
+  }
+
+  return isFieldValid;
+}
